e49e8fd4df5e6b3316a8a45b128798e30b6ef54c
suzannelus <suzannelustenhouwer@gmail.com>
2024-06-10 16:29:59 +0200

    attributionView

---
+++ Attribution.swift
@@ -0,0 +1,18 @@
+//
+//  Attribution.swift
+//  KidsWeather
+//
+//  Created by Suzanne Lustenhouwer on 10/06/2024.
+//
+
+import SwiftUI
+
+struct Attribution: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Attribution()
+}
