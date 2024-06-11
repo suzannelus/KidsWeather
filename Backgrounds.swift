f6ea00d2199b7a33a78c1e7f39a7615a76ec2c15
suzannelus <suzannelustenhouwer@gmail.com>
2024-06-11 21:49:52 +0200

    Added images

---
+++ Backgrounds.swift
@@ -0,0 +1,18 @@
+//
+//  Backgrounds.swift
+//  KidsWeather
+//
+//  Created by Suzanne Lustenhouwer on 10/06/2024.
+//
+
+import SwiftUI
+
+struct Backgrounds: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Backgrounds()
+}
